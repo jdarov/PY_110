3f6193539cade498ba62f6fcaf7d665f9198d1f1
Joshua Darovitz <joshdarovitz@gmail.com>
2025-06-21 07:43:16 -0400

    Completed exercise problem for Medium difficulty

--- exercises/Medium_2/next_featured.py
+++ exercises/Medium_2/next_featured.py
@@ -4,8 +4,33 @@ O
 D
 E
 """
+def digits_repeat(num):
+    string_num = str(num)
+    for digit in string_num:
+        if string_num.count(digit) != 1:
+            return True
+    return False
 
-people = {"Jack":30, "John": 25, "Betty": 25, "Anna":30}
-sorted_people = sorted(people.items(), key=lambda item: (item[1], item[0]))
-print(sorted_people)
-# [('Betty', 25), ('John', 25), ('Anna', 30), ('Jack', 30)]
+def next_featured(number):
+    error = ("There is no possible number that "
+         "fulfills those requirements.")
+    
+    while number <= 9876543201:
+        number += 1
+        if number % 7 == 0 and number % 2 == 1 and not digits_repeat(number):
+            return number
+    return error
+
+print(next_featured(12) == 21)                  # True
+print(next_featured(20) == 21)                  # True
+print(next_featured(21) == 35)                  # True
+print(next_featured(997) == 1029)               # True
+print(next_featured(1029) == 1043)              # True
+print(next_featured(999999) == 1023547)         # True
+print(next_featured(999999987) == 1023456987)   # True
+print(next_featured(9876543186) == 9876543201)  # True
+print(next_featured(9876543200) == 9876543201)  # True
+
+error = ("There is no possible number that "
+         "fulfills those requirements.")
+print(next_featured(9876543201) == error)       # True
